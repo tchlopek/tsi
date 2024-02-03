2618e86626ef340d64fa705ec175359d8eeedee6
Tomasz Chłopek <tomasz.chlopek.dev@gmail.com>
2024-02-03 19:57:50 +0100

    Implement zipped_range

---
+++ test/zipped_range_test.cpp
@@ -0,0 +1,37 @@
+#include <vector>
+
+#include <cppiter/zip.hpp>
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+using namespace testing;
+using cppiter::zip;
+
+template<typename... ts>
+auto Tuple(const ts&... val) {
+  return Eq(std::tuple{ val... });
+}
+
+class ZippedRangeTest : public Test {
+public:
+  std::vector<int> v1{ 1, 2, 3, 4 };
+  std::vector<int> v2{ 10, 11, 12, 13 };
+};
+
+TEST_F(ZippedRangeTest, ShouldIterateOverElements) {
+  ASSERT_THAT(
+    zip(v1, v2),
+    ElementsAre(Tuple(1, 10), Tuple(2, 11), Tuple(3, 12), Tuple(4, 13))
+  );
+}
+
+TEST_F(ZippedRangeTest, ShouldNotExceedMinimalRange) {
+  v1.insert(v1.end(), v2.begin(), v2.end());
+  auto zipped = zip(v1, v2);
+  EXPECT_EQ(zipped.size(), 4);
+  ASSERT_THAT(
+    zipped,
+    ElementsAre(Tuple(1, 10), Tuple(2, 11), Tuple(3, 12), Tuple(4, 13))
+  );
+}
